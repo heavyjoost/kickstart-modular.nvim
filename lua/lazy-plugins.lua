fdcf8616cc642e51e56d08a74d643cc7af4aba1c
HeavyJoost <99620290+heavyjoost@users.noreply.github.com>
2024-01-03 21:53:39 -0700

    Make telescope-fzf-native work on FreeBSD

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -175,9 +175,9 @@ require('lazy').setup({
         'nvim-telescope/telescope-fzf-native.nvim',
         -- NOTE: If you are having trouble with this installation,
         --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
+        build = 'gmake',
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable 'gmake' == 1
         end,
       },
     },
