8dc3baafc6b0680346d18ab14431e5b938a5512c
HeavyJoost <99620290+heavyjoost@users.noreply.github.com>
2024-03-25 20:21:05 -0600

    feat: update colorscheme (tweaked gruvbox)

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,29 +1,49 @@
 return {
-  'iagorrr/noctishc.nvim',
-  'tomasiser/vim-code-dark',
   {
-    'bluz71/vim-moonfly-colors',
-    name = "moonfly",
-  },
-  {
-    'Mofiqul/vscode.nvim',
-    opts = {
-      -- Higher contrast
-      color_overrides = {
-        vscFront = '#FFFFFF',
-        vscBack = '#000000',
-        vscPopupFront = '#FFFFFF',
-        vscContext = '#FFFFFF',
-        vscContextCurrent = '#FFFFFF',
-      }
-    }
-  },
-  {
-    "rockyzhang24/arctic.nvim",
-    branch = "v2",
-    dependencies = { "rktjmp/lush.nvim" },
+    'ellisonleao/gruvbox.nvim',
     config = function()
-      vim.cmd.colorscheme 'my-arctic'
+      require("gruvbox").setup({
+        contrast = "hard",
+        palette_overrides = {
+          dark0_hard = "#000000",
+        },
+        overrides = {
+          TelescopeSelection = {
+            -- link = "GruvboxOrangeBold",
+            fg = "#fe8019",
+            bold = true,
+            bg = "#332e2a",
+          },
+        },
+      })
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
+  -- 'iagorrr/noctishc.nvim',
+  -- 'tomasiser/vim-code-dark',
+  -- {
+  --   'bluz71/vim-moonfly-colors',
+  --   name = "moonfly",
+  -- },
+  -- {
+  --   'Mofiqul/vscode.nvim',
+  --   opts = {
+  --     -- Higher contrast
+  --     color_overrides = {
+  --       vscFront = '#FFFFFF',
+  --       vscBack = '#000000',
+  --       vscPopupFront = '#FFFFFF',
+  --       vscContext = '#FFFFFF',
+  --       vscContextCurrent = '#FFFFFF',
+  --     }
+  --   }
+  -- },
+  -- {
+  --   "rockyzhang24/arctic.nvim",
+  --   branch = "v2",
+  --   dependencies = { "rktjmp/lush.nvim" },
+  --   config = function()
+  --     -- vim.cmd.colorscheme 'my-arctic'
+  --   end,
+  -- },
 }
