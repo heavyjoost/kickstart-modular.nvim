761901f2bf3677eed62952a3dd1f1654646b50a0
HeavyJoost <99620290+heavyjoost@users.noreply.github.com>
2024-03-26 20:55:45 -0600

    feat: add my-arctic colorscheme
    This is currently unused

---
+++ colors/my-arctic.lua
@@ -0,0 +1,21 @@
+vim.opt.background = 'dark'
+vim.g.colors_name = 'my-arctic'
+
+-- First we will need lush, and the colorscheme we wish to modify
+local lush = require('lush')
+local arctic = require('lush_theme.arctic')
+
+local norm_bg = '#000000'
+
+-- we can apply modifications ontop of the existing colorscheme
+local spec = lush.extends({arctic}).with(function()
+  return {
+    Cursor { fg = arctic.Cursor.fg, bg = norm_bg, gui = arctic.Cursor.gui },
+    Normal { fg = arctic.Normal.fg, bg = norm_bg, gui = arctic.Normal.gui },
+    Todo { fg = norm_bg, bg = arctic.Todo.fg, gui = arctic.Todo.gui },
+    UnnecessaryCode { fg = '#6892a9', bg = arctic.UnnecessaryCode.fg, gui = arctic.UnnecessaryCode.gui },
+  }
+end)
+
+-- then pass the extended spec to lush for application
+lush(spec)
